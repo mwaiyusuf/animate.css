bff2d52e3f05fb670a62160afb46124b5176fc2a
mohammed yusuf <mwaigalo5@gmail.com>
2018-06-05 11:03:08 +0300

    t

--- js/custom.js
+++ js/custom.js
@@ -5,5 +5,5 @@ function(){
 var t = document.getElementById("table9");
 2 var cells = t.getElementsByTagName("td");
 3 for (var i = 0; i < cells.length; i++) {
-//4   cells[i].style.backgroundColor = "pink";
+4   cells[i].style.backgroundColor = "pink";
 //5 }
