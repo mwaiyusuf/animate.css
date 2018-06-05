b33dca8c1b263e674817b57f190015d229a9433e
mohammed yusuf <mwaigalo5@gmail.com>
2018-06-05 11:00:40 +0300

    g

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ function(){
     var x=prompt("Please enter your name");
     document.write("Hallo " + name);
 }
-//var t = document.getElementById("table9");
+var t = document.getElementById("table9");
 //2 var cells = t.getElementsByTagName("td");
 //3 for (var i = 0; i < cells.length; i++) {
 //4   cells[i].style.backgroundColor = "pink";
