abd0cda1b74b48e6ef4f8b0ef6ddd33e179cfcf2
mohammed yusuf <mwaigalo5@gmail.com>
2018-06-06 21:27:39 +0300

    g

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
-function(){
-    var x=prompt("Please enter your name");
-    document.write("Hallo " + name);
-}
-var t = document.getElementById("table9");
-2 var cells = t.getElementsByTagName("td");
-3 for (var i = 0; i < cells.length; i++) {
-4   cells[i].style.backgroundColor = "pink";
-5 }
+//function(){
+//    var x=prompt("Please enter your name");
+//    document.write("Hallo " + name);
+//}
+//var t = document.getElementById("table9");
+//2 var cells = t.getElementsByTagName("td");
+//3 for (var i = 0; i < cells.length; i++) {
+//4   cells[i].style.backgroundColor = "pink";
+//5 }
