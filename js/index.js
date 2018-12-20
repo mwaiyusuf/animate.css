7bfc488d97f9c3ec701bbfa5269dca5b16a904a2
mwaiyusuf <mwaigalo5@gmail.com>
2018-12-20 12:14:30 +0300

    G

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res){
-   res.send("Hallo  to the world!");
-});
-
-app.listen(3000);
+//var express = require('express');
+//var app = express();
+//
+//app.get('/', function(req, res){
+//   res.send("Hallo  to the world!");
+//});
+//
+//app.listen(3000);
