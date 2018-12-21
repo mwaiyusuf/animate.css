e8501a469859fb91a76c9a6b1677d0a1920963f6
mwaiyusuf <mwaigalo5@gmail.com>
2018-12-21 23:31:48 +0300

    g

--- js/index.js
+++ js/index.js
@@ -7,10 +7,10 @@
 //
 //app.listen(3000);
 function posneg(){
-var numbers=prompt("Enter not more than four 4 numbers");
+var numbers=prompt("Enter not more than four 4 numbers.");
     for(i=0; i<=4; i++){
         
     }
     
 }
-posneg()
+posneg();
